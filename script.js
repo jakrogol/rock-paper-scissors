8f5d785d45f70fcac3f9219027cda22fe0b12859
Jacob Krogol <jacob@krogol.com>
2024-07-08 11:03:05 -0400

    Provide logic to adjudicate a round of rock-paper-scissors.

--- script.js
+++ script.js
@@ -11,3 +11,33 @@ function getHumanChoice() {
   let choice = prompt("Please make a selection. 0 = Rock, 1 = Paper, 2 = Scissors");
   return options[choice];
 }
+
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice == 'rock' && computerChoice == 'paper') {
+    console.log("You lose! Paper beats rock.");
+    computerScore++;
+  }
+  else if (humanChoice == 'rock' && computerChoice == 'scissors') {
+    console.log("You win! Rock beats scissors!");
+    humanScore++;
+  }
+  else if (humanChoice == 'paper' && computerChoice == 'rock') {
+    console.log("You win! Paper beats rock!");
+    humanScore++;
+  }
+  else if (humanChoice == 'paper' && computerChoice == 'scissors') {
+    console.log("You lose! Scissors beats paper.");
+    computerScore++;
+  }
+  else if (humanChoice == 'scissors' && computerChoice == 'rock') {
+    console.log("You lose! Rock beats scissors.");
+    computerScore++;
+  }
+  else if (humanChoice == 'scissors' && computerChoice == 'paper') {
+    console.log("You win! Scissors beats paper!");
+  }
+  else {
+    console.log(`You threw ${humanChoice} and the computer ALSO threw ${computerChoice}. Tie!`);
+  }
+  console.log(`Current score - You: ${humanScore}, Them: ${computerScore}`);
+}
