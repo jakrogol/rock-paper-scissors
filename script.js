58f62842fdb683d92d0bddcf04866504c8073ad1
Jacob Krogol <jacob@krogol.com>
2024-07-17 13:58:57 -0400

    Refactor moves as objects and declare DOM target consts.

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
-const options = ['rock', 'paper', 'scissors'];
+const moves = [
+  {id: 'rock', emoji: '✊'},
+  {id: 'paper', emoji: '✋'},
+  {id: 'scissors', emoji: '✌️'},
+];
 
 const buttons = document.querySelectorAll('button');
 const results = document.querySelector('#results');
 
-let humanScore = 0;
-let computerScore = 0;
+const scoreboardHeading = document.getElementById("scoreboard-heading");
+const scoreboardMsg = document.getElementById("scoreboard-message");
+const humanMove = document.getElementById("human-move");
+const humanScore = document.getElementById("human-score");
+const computerMove = document.getElementById("computer-move");
+const computerScore = document.getElementById("computer-score");
+
+let humanPoints = 0;
+let computerPoints = 0;
 
 buttons.forEach(button => {
   button.addEventListener('click', () => playRound(button.id));
@@ -15,48 +26,51 @@ function playRound(humanChoice) {
   const resultMsg = document.createElement("p");
   const currentScore = document.createElement("p");
 
-  if ((humanChoice == 'rock' && computerChoice == 'scissors')
-    || (humanChoice == 'paper' && computerChoice == 'rock')
-    || (humanChoice == 'scissors' && computerChoice == 'paper')) {
-      resultMsg.textContent = `${capitalize(humanChoice)} beats ${computerChoice}! You get a point!`;
+  humanMove.textContent = moves.find(m => m.id === humanChoice).emoji;
+  computerMove.textContent = computerChoice.emoji;
+
+  if ((humanChoice == 'rock' && computerChoice.id == 'scissors')
+    || (humanChoice == 'paper' && computerChoice.id == 'rock')
+    || (humanChoice == 'scissors' && computerChoice.id == 'paper')) {
+      resultMsg.textContent = `${capitalize(humanChoice)} beats ${computerChoice.id}! You get a point!`;
       results.appendChild(resultMsg);
-      humanScore++;
+      humanPoints++;
   }
-  else if ((computerChoice == 'rock' && humanChoice == 'scissors')
-    || (computerChoice == 'paper' && humanChoice == 'rock')
-    || (computerChoice == 'scissors' && humanChoice == 'paper')) {
-      resultMsg.textContent = `${capitalize(computerChoice)} beats ${humanChoice}. Computer gets a point.`;
+  else if ((computerChoice.id == 'rock' && humanChoice == 'scissors')
+    || (computerChoice.id == 'paper' && humanChoice == 'rock')
+    || (computerChoice.id == 'scissors' && humanChoice == 'paper')) {
+      resultMsg.textContent = `${capitalize(computerChoice.id)} beats ${humanChoice}. Computer gets a point.`;
       results.appendChild(resultMsg);
-      computerScore++;
+      computerPoints++;
   }
   else {
-    resultMsg.textContent = `You threw ${humanChoice} and the computer ALSO threw ${computerChoice}. Tie!`
+    resultMsg.textContent = `You threw ${humanChoice} and the computer ALSO threw ${computerChoice.id}. Tie!`
     results.appendChild(resultMsg);
   }
 
-  if (humanScore == 5 || computerScore == 5) {
+  if (humanPoints == 5 || computerPoints == 5) {
     const finalMsg = document.createElement("div");
     buttons.forEach((button) => {
       button.disabled = true;
     });
 
     if (humanScore > computerScore) {
-      finalMsg.textContent = `You win! Final score = ${humanScore} to ${computerScore}`;
+      finalMsg.textContent = `You win! Final score = ${humanPoints} to ${computerPoints}`;
       results.appendChild(finalMsg);
     }
     else {
-      finalMsg.textContent = `You lose... Final score = ${humanScore} to ${computerScore}`;
+      finalMsg.textContent = `You lose... Final score = ${humanPoints} to ${computerPoints}`;
       results.appendChild(finalMsg);
     }
   }
   else {
-    currentScore.textContent = `Current score - You: ${humanScore}, Them: ${computerScore}`;
+    currentScore.textContent = `Current score - You: ${humanPoints}, Them: ${computerPoints}`;
     results.appendChild(currentScore);
   }
 }
 
 function getComputerChoice() {
-  return options[Math.floor(Math.random() * 3)];
+  return moves[Math.floor(Math.random() * 3)];
 }
 
 function capitalize(string) {
